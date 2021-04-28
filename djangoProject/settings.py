8a8c42acf1f6e9ce12eba0f7eafc4cc7ec1416dc
Reventlow <gorm@reventlow.com>
2021-04-28 15:16:04 +0200

    added 0.0.0.0 to host

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -131,9 +131,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #
